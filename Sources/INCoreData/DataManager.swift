fbbcadb1e4e2b7dc14b8c3ad16fc2a601185dba4
Hüseyin <huseyinbagana@yandex.com>
2022-05-07 15:40:54 +0300

    latest fixes.

--- Sources/INCoreData/DataManager.swift
+++ Sources/INCoreData/DataManager.swift
@@ -1,13 +1,8 @@
-//
-//  INCoreDataBridge.swift
-//  INCoreData
-//
-//  Created by Hüseyin Bagana on 11.04.2022.
-//
-
 import CoreData
 import Foundation
 
-// That class will prove that we can user CoreData subclasses'
-// inside another application when we had linked INCoreData
+/*
+ That class will prove that we can user CoreData subclasses'
+ inside another application when we had linked INCoreData
+ */
 public class DataManager: NSPersistentContainer {}
