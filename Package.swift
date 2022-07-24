0fddce105723e9f4bc202c481a07d56e90dc5baa
Sven Korset <sven.korset@indie-software.com>
2022-07-24 08:14:32 +0200

    Add dependencies

--- Package.swift
+++ Package.swift
@@ -21,9 +21,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "INCoreData",
-            dependencies: []),
+            dependencies: ["CoreData"]),
         .testTarget(
             name: "INCoreDataTests",
-            dependencies: ["INCoreData"]),
+            dependencies: ["INCoreData","CoreData"]),
     ]
 )
