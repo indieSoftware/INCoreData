31212d95db2eacb0ee12a8fd36ba7880adad3887
Sven Korset <sven.korset@indie-software.com>
2022-07-24 08:24:13 +0200

    Reverted CoreData dependency in SPM

--- Package.swift
+++ Package.swift
@@ -21,9 +21,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "INCoreData",
-            dependencies: ["CoreData"]),
+            dependencies: []),
         .testTarget(
             name: "INCoreDataTests",
-            dependencies: ["INCoreData","CoreData"]),
+            dependencies: ["INCoreData"]),
     ]
 )
